00432512b780c74d0d8cf4175341244041087322
Gargi-1234 <87595595+Gargi-1234@users.noreply.github.com>
2023-01-25 18:06:14 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,12 @@
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAvWooRK3K-t9nimq3UbQA2VFWbB9reCps",
+    authDomain: "storytellingapp-d28e5.firebaseapp.com",
+    projectId: "storytellingapp-d28e5",
+    storageBucket: "storytellingapp-d28e5.appspot.com",
+    messagingSenderId: "425160648305",
+    appId: "1:425160648305:web:66887b0001ad36e6289dd0"
+  };
+
+  
